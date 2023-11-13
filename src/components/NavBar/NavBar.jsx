44eefd3ef31e82f535905d3dda2b339135b83623
diegogieser <67288394+diegogieser@users.noreply.github.com>
2023-11-13 09:16:45 -0300

    Logo
    Logo empresa, hay que modificarlo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import CartWidget from '../CartWidget/CartWidget';
-
+import Logo from '../../assets/logo.jfif';
 
 const NavBar = () => {
   return (
     <div className="navbar">
+      <img src={Logo} alt="Logo de OuRouge" className="logo" />  {/* Agrega la etiqueta img para el logo */}
       <h1>Fragancias OuRouge</h1>
       <div className="nav-links">
         <button>Inicio</button>
